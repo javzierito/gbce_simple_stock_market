acd96c8716fd63d4398155072a49507f7dbcbda5
Javier_vazquez <javiervazper@gmail.com>
2025-01-30 23:29:46 +0100

    defined tests for stock class, including some corner and ill defined cases
    fixed test input data to make my life easier testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,38 +1,50 @@
 import pytest
 
+from src.stock import Stock
+
 
 @pytest.fixture
 def sample_data_gbce():
     stocks_of_market = {
         "TEA": {
-            "Type": "COMMON",
+            "type": "COMMON",
             "last_dividend": 0,
             "fixed_dividend": None,
-            "Par Value": 100
+            "par_value": 100
             },
         "POP": {
-            "Type": "COMMON",
+            "type": "COMMON",
             "last_dividend": 8,
             "fixed_dividend": None,
-            "Par Value": 100
+            "par_value": 100
             },
         "ALE": {
-            "Type": "COMMON",
+            "type": "COMMON",
             "last_dividend": 23,
             "fixed_dividend": None,
-            "Par Value": 60
+            "par_value": 60
         },
         "GIN": {
-            "Type": "PREFERRED",
+            "type": "PREFERRED",
             "last_dividend": 8,
             "fixed_dividend": 0.02,
-            "Par Value": 100
+            "par_value": 100
         },
         "JOE": {
-            "Type": "COMMON",
+            "type": "COMMON",
             "last_dividend": 13,
             "fixed_dividend": None,
             "par_value": 250
         }
     }
     return stocks_of_market
+
+
+@pytest.fixture
+def get_stock_instances(sample_data_gbce):
+    stocks = []
+    for key, values in sample_data_gbce.items():
+        stock_instance = Stock(symbol=key, **values)
+        stocks.append(stock_instance)
+
+    return stocks
---
+++ tests/test_stock.py
@@ -0,0 +1,25 @@
+import pytest
+
+
+def test_negative_and_zero_price_value(get_stock_instances, caplog):
+    second_stock = get_stock_instances[1]
+    with pytest.raises(ValueError):
+        second_stock.dividend_yield(0)
+        second_stock.dividend_yield(-20)
+
+
+def test_pe_ratio_normal_values(get_stock_instances):
+    stock_prices = [20, 23, 34, 50, 40]
+    resultant_dividend_and_pr = [
+        [0.0, float('inf')],
+        [0.34782608695652173, 2.875],
+        [0.6764705882352942, 1.4782608695652173],
+        [0.04, 25.0], [0.325, 3.076923076923077]
+    ]
+    dividend_and_pr = []
+    for price, stock in zip(stock_prices, get_stock_instances):
+        dividend_and_pr.append(
+            [stock.dividend_yield(price), stock.pe_ratio(price)]
+        )
+    # REVIEW JAVIER: change the approach to math.isclose()
+    assert dividend_and_pr == resultant_dividend_and_pr
