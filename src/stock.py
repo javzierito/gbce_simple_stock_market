74e7b356f00c38653cd0858e4427f220442b47fa
Javier_vazquez <javiervazper@gmail.com>
2025-02-01 18:01:48 +0100

    implemented DRY principles for stock classes and stock factory for creation
    the stock class creation should be easy to generalize for other stock types

--- src/stock.py
+++ src/stock.py
@@ -1,33 +1,69 @@
-from dataclasses import dataclass
-from enum import Enum
 from typing import Union
-
-
-class StockType(Enum):
-    COMMON = "COMMON"
-    PREFERRED = "PREFERRED"
+from abc import ABC, abstractmethod
+from pydantic import ValidationError
+from pydantic.dataclasses import dataclass
 
 
 @dataclass
-class Stock:
+class BaseStock(ABC):
     symbol: str
-    type: StockType
     last_dividend: float
-    fixed_dividend: Union[str, None]
     par_value: float
 
-    def dividend_yield(self, price: float) -> float:
+    @staticmethod
+    def validate_price(price: float) -> None:
         if price <= 0:
-            raise ValueError("with a price of 0 or inferior the div yields loses its meaning")
-        if self.type == StockType.COMMON.value:
-            return self.last_dividend / price
-        elif self.type == StockType.PREFERRED.value:
-            fix_dividend = float(self.fixed_dividend.replace('%', '')) / 100
-            return self.par_value * fix_dividend / price
-        else:
-            raise ValueError(f"Invalid stock type: {self.type}")
+            raise ValueError("Price must be greater than 0 for dividend yield calculation.")
 
-    def pe_ratio(self, price: float) -> float:
+    @abstractmethod
+    def _dividend_yield(self, price: float) -> float:
+        pass
+
+    def dividend_yield(self, price: float) -> float:
+        self.validate_price(price)
+        return self._dividend_yield(price)
+
+    def calculate_pe_ratio(self, price: float) -> float:
         dividend = self.dividend_yield(price) * price
         return price / dividend if dividend > 0 else float("inf")
+
+
+@dataclass
+class PreferredStock(BaseStock):
+    fixed_dividend: float
+
+    def _calculate_dividend_yield(self, price: float) -> float:
+        fix_dividend = self.fixed_dividend / 100
+        return self.par_value * fix_dividend / price
+
+
+class CommonStock(BaseStock):
+    def _calculate_dividend_yield(self, price: float) -> float:
+        return self.last_dividend / price
+
+
+stock_type_vs_class = {
+    "COMMON": CommonStock,
+    "PREFERRED": PreferredStock,
+}
+
+
+def filter_stock_attrs(stock_attrs: dict, stock_klass: BaseStock):
+    klass_members = stock_klass.__annotations__
+    return {key: stock_attrs[key] for key in klass_members}
+
+
+def stock_factory(stock_type: str, stock_attrs: dict) -> Union[PreferredStock, CommonStock, None]:
+    stock_klass = stock_type_vs_class.get(stock_type, "")
+    stock_instance = None
+    if not stock_klass:
+        raise ValueError(f"That stock type {stock_type} is not implemented")
+    try:
+        filtered_stock_attrs = filter_stock_attrs(stock_attrs, stock_klass)
+        stock_instance = stock_klass(**filtered_stock_attrs)
+    except ValidationError as e:
+        print(e.errors())
+    except TypeError as e:
+        print(e)
+    return stock_instance
 
